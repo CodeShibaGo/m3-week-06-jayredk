db836d4bc7f306a2d872e6a4865f4664273a148b
Jay <kred110396@gmail.com>
2024-02-14 00:45:09 +0800

    feat: add avatar for profile page

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -4,6 +4,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 from app import db, login
 
 @login.user_loader
@@ -29,6 +30,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
